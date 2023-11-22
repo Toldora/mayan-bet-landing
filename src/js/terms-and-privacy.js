e5b7dfdf353bc5d950f8098ffaca014424845466
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-11-22 15:50:08 +0200

    sidebar

--- src/js/terms-and-privacy.js
+++ src/js/terms-and-privacy.js
@@ -4,8 +4,8 @@ import template from '@/partials/terms.html?raw';
 
 const markup = handlebars.compile(template)();
 
-const termsBtnRef = document.querySelector('.js-terms-btn');
-const policyBtnRef = document.querySelector('.js-privacy-policy-btn');
+const termsBtnRefs = document.querySelectorAll('.js-terms-btn');
+const policyBtnRefs = document.querySelectorAll('.js-privacy-policy-btn');
 const modalContentRef = document.querySelector('.js-app-modal-content');
 
 const onOpenModal = () => {
@@ -14,5 +14,10 @@ const onOpenModal = () => {
   openModal();
 };
 
-termsBtnRef.addEventListener('click', onOpenModal);
-policyBtnRef.addEventListener('click', onOpenModal);
+[...termsBtnRefs].forEach(ref => {
+  ref.addEventListener('click', onOpenModal);
+});
+
+[...policyBtnRefs].forEach(ref => {
+  ref.addEventListener('click', onOpenModal);
+});
